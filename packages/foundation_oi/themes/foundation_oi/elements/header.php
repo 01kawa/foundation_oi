39db80b2491c7118522536924e908325441fe39f
01kawa <kmgrbgm@gmail>
2015-05-23 06:25:12 +0900

    style.css changed style.less full page

--- packages/foundation_oi/themes/foundation_oi/elements/header.php
+++ packages/foundation_oi/themes/foundation_oi/elements/header.php
@@ -5,11 +5,23 @@ defined('C5_EXECUTE') or die("Access Denied.");
 <html class="" lang="<?php echo Localization::activeLanguage()?>">
   <head>
     <?php Loader::element('header_required', array('pageTitle' => $pageTitle));?>
-    <link rel="stylesheet" href="<?=$view->getThemePath()?>/css/style.css" />
-    <link rel="stylesheet" href="<?=$view->getThemePath()?>/css/normalize.css" />
-    <link rel="stylesheet" href="<?=$view->getThemePath()?>/css/foundation.css" />
+    <link href="<?=$view->getThemePath()?>/css/normalize.css" rel="stylesheet">
+    <link href="<?=$view->getThemePath()?>/css/foundation.css" rel="stylesheet">
+    <link href="<?=$view->getStylesheet('style.less')?>" rel='stylesheet' type='text/css'>
     <script src="<?=$view->getThemePath()?>/js/vendor/modernizr.js"></script>
   </head>
   <body>
 
     <div class="<?php echo $c->getPageWrapperClass()?>">
+
+		<div class="row">
+		  
+		    <div class="large-12 columns">
+		      <?php
+		          $a = new GlobalArea('Navigation');
+		          $a->setAreaGridMaximumColumns(12);
+		          $a->display($c);
+		      ?>
+		    </div>
+
+		</div>
--- packages/foundation_oi/themes/foundation_oi/full.php
+++ packages/foundation_oi/themes/foundation_oi/full.php
@@ -4,17 +4,9 @@ $this->inc('elements/header.php'); ?>
 
   <div class="row">
 
-    <div class="large-8 columns">
+    <div class="large-12 columns">
       <?php
           $a = new Area('Main');
-          $a->setAreaGridMaximumColumns(12);
-          $a->display($c);
-      ?>
-    </div>
-
-    <div class="large-4 columns">
-      <?php
-          $a = new Area('Sidebar');
           $a->setAreaGridMaximumColumns(12);
           $a->display($c);
       ?>
